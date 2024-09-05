facfa55ba34659d3f34e883fdb57b86b03a6c5e5
Adhil Shan A P <66194129+adhilshan@users.noreply.github.com>
2024-09-05 14:55:14 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,54 +1,22 @@
 from flask import Flask, request
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime, timedelta
 import time
 import json
-import os
 
 app = Flask(__name__)
 
 data_array = []
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-with open(os.path.join(current_dir, 'database.json'), 'r') as file:
-    data_array = json.load(file)
 
 places = [
   "Agra", "Ahmedabad", "Allahabad", "Aurangabad", "Bangalore", "Bhopal", "Bhubaneswar", "Chandigarh", "Chennai", "Coimbatore", "Dehradun", "Erode", "Faridabad", "Ghaziabad", "Gurgaon", "Guwahati", "Hyderabad", "Indore", "Jaipur", "Jammu", "Jamshedpur", "Kanpur Urban", "Kolhapur", "Kolkata", "Kozhikode", "Lucknow", "Ludhiana", "Madurai", "Mangalore", "Mumbai", "Mysore", "Nagpur", "Nashik", "New Delhi", "Noida", "Patna", "Pune", "Raipur", "Rajkot", "Ranchi", "Salem", "Shimla", "Srinagar", "Surat", "Thane", "Tiruchchirappalli", "Trivandrum", "Vadodara", "Varanasi", "Visakhapatnam"
 ]
 
-@app.route('/api/diesel-rate/all')
-def all():
-    if data_array != []:
-        unix_timestamp = data_array[0]["timestamp"]
-        timestamp = datetime.fromtimestamp(unix_timestamp)
-        current_time = datetime.now()
-        time_24_hours_ago = current_time - timedelta(days=1)
-        if time_24_hours_ago <= timestamp <= current_time:
-            return data_array
-        else:
-            getPrice()
-            return data_array
-    else:
-        getPrice()
-    return data_array
-
 @app.route('/api/diesel-rate')
 def home():
-    if data_array != []:
-        unix_timestamp = data_array[0]["timestamp"]
-        timestamp = datetime.fromtimestamp(unix_timestamp)
-        current_time = datetime.now()
-        time_24_hours_ago = current_time - timedelta(days=1)
-        if time_24_hours_ago <= timestamp <= current_time:
-            return data_array[len(data_array) - 1]
-        else:
-            getPrice()
-            return data_array[len(data_array) - 1]
-    else:
-        getPrice()
+    getPrice()
     return data_array[len(data_array) - 1]
+
 def getPrice():
     data_array_2 = []
     for place in places:
@@ -81,5 +49,3 @@ def getPrice():
     JSON_OBJ_1['data'] = data_array_2
     JSON_OBJ_1['meassure'] = 'INR/litre'
     data_array.append(JSON_OBJ_1)
-    with open(os.path.join(current_dir, 'database.json'), 'w') as file:
-        json.dump(data_array, file, indent=4)
