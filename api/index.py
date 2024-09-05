bf333a3f4484d545be2c5c954395d4af4960a8fa
Adhil Shan A P <66194129+adhilshan@users.noreply.github.com>
2024-09-05 15:22:49 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,28 +1,36 @@
 import logging
-from flask import Flask, request
+from flask import Flask, jsonify
 import requests
 from bs4 import BeautifulSoup
+import time
 
 logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
+places = [
+    "Agra", "Ahmedabad", "Allahabad", "Aurangabad", "Bangalore", "Bhopal", "Bhubaneswar", "Chandigarh", 
+    "Chennai", "Coimbatore", "Dehradun", "Erode", "Faridabad", "Ghaziabad", "Gurgaon", "Guwahati", 
+    "Hyderabad", "Indore", "Jaipur", "Jammu", "Jamshedpur", "Kanpur Urban", "Kolhapur", "Kolkata", 
+    "Kozhikode", "Lucknow", "Ludhiana", "Madurai", "Mangalore", "Mumbai", "Mysore", "Nagpur", "Nashik", 
+    "New Delhi", "Noida", "Patna", "Pune", "Raipur", "Rajkot", "Ranchi", "Salem", "Shimla", "Srinagar", 
+    "Surat", "Thane", "Tiruchchirappalli", "Trivandrum", "Vadodara", "Varanasi", "Visakhapatnam"
+]
+
 @app.route('/api/diesel-rate')
 def home():
     try:
-        return getPrice()
+        return jsonify(getPrice())
     except Exception as e:
         logging.error(f"An error occurred: {e}")
-        return {"error": "An internal error occurred"}, 500
+        return jsonify({"error": "An internal error occurred"}), 500
 
 def getPrice():
     try:
-        data_array = []
         data_array_2 = []
         for place in places:
-            JSON_OBJ_1 = {}
             JSON_OBJ_2 = {}
-            url = 'https://www.financialexpress.com/diesel-rate-in-'+ place +'/#main-heading'
+            url = f'https://www.financialexpress.com/diesel-rate-in-{place}/#main-heading'
 
             headers = {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.138 Safari/537.36'
@@ -43,9 +51,11 @@ def getPrice():
                     logging.warning(f"₹ symbol not found in the text for {place}.")
             data_array_2.append(JSON_OBJ_2)
 
-        JSON_OBJ_1['timestamp'] = time.time()
-        JSON_OBJ_1['data'] = data_array_2
-        JSON_OBJ_1['measure'] = 'INR/litre'
+        JSON_OBJ_1 = {
+            'timestamp': time.time(),
+            'data': data_array_2,
+            'measure': 'INR/litre'
+        }
         return JSON_OBJ_1
 
     except requests.RequestException as e:
